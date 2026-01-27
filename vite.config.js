ace4ea3945b9d290c9731ab5eb937c695f66478b
Allan Felipe dos Anjos <Allan.Anjos@mpcwb.intranet>
2026-01-27 15:39:45 -0300

    Fix: Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/gerador-fundo-fixo/',
   plugins: [
     react(),
     tailwindcss(),
